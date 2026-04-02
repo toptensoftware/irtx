205d37d8459b9c2f98f7ab594e403ad2b7beccdb
Brad Robinson <brad@toptensoftware.com>
2026-04-02 19:44:09 +1100

    Fix using wrong device url in production

--- webui/config.js
+++ webui/config.js
@@ -3,5 +3,5 @@ export let config = {
     description: "irtx IR Blaster",
     // In production the UI is served from the device (same-origin), so deviceUrl should be "".
     // During development, point to the device IP.
-    deviceUrl: "http://10.1.1.101",
+    deviceUrl: "",
 }
